3b4252ab175d0685b0eee183b879e8c7f07ebd83
Sanzhar <sanzhar.saparkhan@mail.ru>
2026-02-23 12:41:54 +0500

    lab5

---
+++ online-store/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,37 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  // Использование нового синтаксиса (preferred)
+  product = input.required<Product>();
+  delete = output<number>();
+
+  likeProduct() {
+    // В реальном приложении мы бы вызывали API, здесь просто меняем свойство объекта
+    this.product().likes++;
+  }
+
+  onDelete() {
+    const confirmDelete = confirm(`Вы уверены, что хотите удалить ${this.product().name}?`);
+    if (confirmDelete) {
+      this.delete.emit(this.product().id);
+    }
+  }
+
+  share(platform: 'whatsapp' | 'telegram') {
+    const url = encodeURIComponent(this.product().link);
+    if (platform === 'whatsapp') {
+      window.open(`https://api.whatsapp.com/send?text=${url}`, '_blank');
+    } else {
+      window.open(`https://t.me/share/url?url=${url}`, '_blank');
+    }
+  }
+}
---
+++ online-store/src/app/models/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  name: string;
+  categoryId: number;
+  description: string;
+  price: number;
+  rating: number;
+  likes: number;
+  image: string;
+  images: string[];
+  link: string;
+} 
